71e16bb721f808ce275df5f67b62017d6692921e
markup54 <marzenakupka@tlen.pl>
2025-12-05 14:14:20 +0100

    podsumowanie

--- app/src/main/java/pl/zabrze/zs10/testoflagach/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/testoflagach/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int licznikPrzyciskow;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewPolecenie);
     }
 
     public void ukryj(View view) {
         view.setVisibility(View.INVISIBLE);
+        licznikPrzyciskow++;
+        if(licznikPrzyciskow == 4){
+            //Toast.makeText(this, "Brawo to jest flaga Polski", Toast.LENGTH_SHORT).show();
+            textView.setText("Brawo to jest flaga Polski!");
+        }
     }
 
     public void ok(View view) {
